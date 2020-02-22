49d063a1b1231e68283000190f2b79d8941c0a10
Jacob Sansbury <sansburyjacob@gmail.com>
2020-02-21 21:04:21 -0800

    Add VSCode configs

---
+++ install/vscode.sh
@@ -0,0 +1,3 @@
+ln -s ~/.me/conf/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/.me/conf/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -s ~/.me/conf/vscode/snippets/ ~/Library/Application\ Support/Code/User
